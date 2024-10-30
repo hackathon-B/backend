c6787707220a27a9896e012a6afa1a831829608f
ARISA-0017 <a.engine0808@gmail.com>
2024-10-31 01:27:38 +0900

    [add]core/config.pyにGoogle認証の設定を追加

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseSettings
+import os
 
 class Settings(BaseSettings):
     # dbコンテナの環境変数
@@ -7,6 +8,11 @@ class Settings(BaseSettings):
     DB_HOST: str = "db"  
     DB_PORT: str = "3306"
     DB_NAME: str = "app_db"
+
+    # Google認証のクライアント情報
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET")
+    GOOGLE_REDIRECT_URI: str = "https://fucabo.net/api/google/callback"
 
     # @propertyはメソッドを属性のようにアクセスできるデコレーター
     @property
