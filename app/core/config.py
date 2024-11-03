a61efe86b4ff125bbc51deefd401ae8b067c5cad
ARISA-0017 <a.engine0808@gmail.com>
2024-11-04 00:35:12 +0900

    [update]core/config.pyを更新

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,4 @@
 from pydantic import BaseSettings
-import os
 
 class Settings(BaseSettings):
     # dbコンテナの環境変数
@@ -10,13 +9,18 @@ class Settings(BaseSettings):
     DB_NAME: str = "app_db"
 
     # Google認証のクライアント情報
-    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
-    GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET")
+    GOOGLE_CLIENT_ID: str
+    GOOGLE_CLIENT_SECRET: str
     GOOGLE_REDIRECT_URI: str = "https://fucabo.net/api/google/callback"
 
     # @propertyはメソッドを属性のようにアクセスできるデコレーター
     @property
     def DATABASE_URL(self) -> str:
         return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    # 環境変数を.envファイルから読み込むように指定
+    class Config:
+        env_file = ".env"
+        env_file_encording = "utf-8"
 
 settings = Settings()
