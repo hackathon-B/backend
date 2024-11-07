e0b49eeb3a7fd882b95c6e02e33458d906f97dbe
Koujirou513 <kojiro323a@gmail.com>
2024-11-07 14:21:51 +0900

    [add] crud/chat.py,チャット取得関数更新

--- app/crud/chat.py
+++ app/crud/chat.py
@@ -1,6 +1,5 @@
 from typing import Optional, List
-
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from app.models.chat import Chat
 from app.schemas.chat import ChatCreate, ChatUpdate
@@ -12,13 +11,27 @@ class CRUDChat(CRUDBase[Chat, ChatCreate, ChatUpdate]):
     def get_by_user(self, db_session: Session, *, user_id: int) -> List[Chat]:
         return db_session.query(Chat).filter(Chat.user_id == user_id).all()
 
-    def get_by_title(self, db_session: Session, *, user_id: int, title: str) -> Optional[Chat]:
-        return db_session.query(Chat).filter(Chat.user_id == user_id, Chat.chat_title == title).first()
+    def get_chat_details(self, db_session: Session, *, chat_id: int) -> Optional[Chat]:
+        # 特定のチャットの情報を取得
+        chat = (
+            db_session.query(Chat)
+            .options(
+                joinedload(Chat.parent_chat),  # 親チャットをロード
+                joinedload(Chat.child_chats),  # 子チャットをロード
+                joinedload(Chat.messages)      # チャットに含まれるメッセージをロード
+            )
+            .filter(Chat.chat_id == chat_id)
+            .first()
+        )
+        return chat 
 
-    def update_chat_title(self, db_session: Session, *, chat_id: int, new_title: str) -> Optional[Chat]:
+    def update_chat(self, db_session: Session, *, chat_id: int, new_title: Optional[str] = None, new_model_id: Optional[int] = None) -> Optional[Chat]:
         chat = db_session.query(Chat).filter(Chat.chat_id == chat_id).first()
         if chat:
-            chat.chat_title = new_title
+            if new_title is not None:
+                chat.chat_title = new_title
+            if new_model_id is not None:
+                chat.model_id = new_model_id
             db_session.commit()
             db_session.refresh(chat)
         return chat
