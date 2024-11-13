1107e4c9c711c70379747f6948b28d940d98a22d
ARISA-0017 <a.engine0808@gmail.com>
2024-11-13 23:30:25 +0900

    [update]schemas/user.pyを更新

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,7 +6,7 @@ class UserBase(BaseModel):
 
 # ユーザー作成時のスキーマを定義
 class UserCreate(UserBase):
-    password: str = Field(..., min_length=8, max_length=16, description="パスワードは8文字以上の長さで設定してください")
+    password: str = Field(..., min_length=8, max_length=16, description="パスワードは8文字以上16文字以内で設定してください。")
 
 # ログイン時のスキーマを定義
 class UserLogin(UserBase):
