be6d8f4a96d1306b9b412db77d3d1e6d94d4287b
Koujirou513 <kojiro323a@gmail.com>
2024-11-13 21:04:33 +0900

    [fix] crud/chat.py 不要なメソッド削除

--- app/crud/chat.py
+++ app/crud/chat.py
@@ -7,13 +7,13 @@ from app.crud.base import CRUDBase
 
 
 class CRUDChat(CRUDBase[Chat, ChatCreate, ChatUpdate]):
-    # 必要に応じて、以下のようにメソッドを加えたり、オーバーライドする。
     def get_by_user(self, db_session: Session, *, user_id: int) -> List[Chat]:
+        """ユーザーに関する全てのチャットを取得"""
         return db_session.query(Chat).filter(Chat.user_id == user_id).all()
 
     def get_chat_details(self, db_session: Session, *, chat_id: int) -> Optional[Chat]:
         # 特定のチャットの情報を取得
-        chat = (
+        return (
             db_session.query(Chat)
             .options(
                 joinedload(Chat.parent_chat),  # 親チャットをロード
@@ -23,25 +23,5 @@ class CRUDChat(CRUDBase[Chat, ChatCreate, ChatUpdate]):
             .filter(Chat.chat_id == chat_id)
             .first()
         )
-        return chat 
-
-    def update_chat(self, db_session: Session, *, chat_id: int, new_title: Optional[str] = None, new_model_id: Optional[int] = None) -> Optional[Chat]:
-        chat = db_session.query(Chat).filter(Chat.chat_id == chat_id).first()
-        if chat:
-            if new_title is not None:
-                chat.chat_title = new_title
-            if new_model_id is not None:
-                chat.model_id = new_model_id
-            db_session.commit()
-            db_session.refresh(chat)
-        return chat
-    
-    def delete_chat(self, db_session: Session, *, chat_id: int) -> bool:
-        chat = db_session.query(Chat).filter(Chat.chat_id == chat_id).first()
-        if chat:
-            db_session.delete(chat)
-            db_session.commit()
-            return True
-        return False
 
 crud_chat = CRUDChat(Chat)
--- app/crud/message.py
+++ app/crud/message.py
@@ -8,11 +8,7 @@ from app.crud.base import CRUDBase
 class CRUDMessage(CRUDBase[Message, MessageCreate, MessageUpdate]):
     def get_by_chat(self, db_session: Session, *, chat_id: int) -> List[Message]:
         # 特定のチャットに関連するすべてのメッセージを取得
-        try:
-            return db_session.query(Message).filter(Message.chat_id == chat_id).all()
-        except Exception as e:
-            db_session.rollback()
-            raise e
+        return db_session.query(Message).filter(Message.chat_id == chat_id).all()
     
 # updateとdeleteはbaseクラスのメソッドで代用可能
     
